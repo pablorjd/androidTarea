e435711e665df35fadcb3cc5a5112b1e57f146c4
Pablo Jiménez <pablo.jimenez2221@gmail.com>
2018-04-19 15:13:09 -0300

    segundo commit

---
+++ app/src/main/java/com/example/pablorjd/cinefilos/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.pablorjd.cinefilos;
+
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView txtTitulo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+
+        txtTitulo = (TextView)findViewById(R.id.txtTitulo);
+    }
+
+
+    public void getData(){
+        String sql = "http://www.omdbapi.com/?i=tt3896198&apikey=35e3ba57";
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        URL url = null;
+        HttpURLConnection conn;
+
+        try {
+            url = new URL(sql);
+            conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("GET");
+
+            conn.connect();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String inputLine;
+
+            StringBuffer response = new StringBuffer();
+
+            String json = "";
+
+            while((inputLine = in.readLine()) != null){
+                response.append(inputLine);
+            }
+
+            json = response.toString();
+
+            JSONArray jsonArr = null;
+
+            jsonArr = new JSONArray(json);
+            String mensaje = "";
+            for(int i = 0;i<jsonArr.length();i++){
+                JSONObject jsonObject = jsonArr.getJSONObject(i);
+
+                Log.d("SLIDA",jsonObject.optString("Title"));
+                mensaje += "DESCRIPCION "+i+" "+jsonObject.optString("Title")+"\n";
+            }
+            txtTitulo.setText(mensaje);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/pablorjd/cinefilos/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.pablorjd.cinefilos;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class );
+                startActivity(intent);
+            }
+        },3000);
+
+    }
+}
